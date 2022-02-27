e697f87a21570e69eb10e11d3ba6a114cb4059c2
WalterBanda <walterkaimurima@gmail.com>
2022-02-27 09:03:05 +0300

    ✈ cpovc_manage : Reformat code for views, urls, forms
    # Changes ✈
    1. Import refactor for views ie from django.core.urlresolvers.reverse to django.urls.reverse and for forms ie from  ugettext_lazy to  gettext_lazy
    2. API refactor from patterns, url to include, path for urls

--- cpovc_manage/urls.py
+++ cpovc_manage/urls.py
@@ -1,24 +1,24 @@
 """Urls for Settings."""
-from django.conf.urls import patterns, url
+from django.urls import include, path
 
 # This should contain urls related to settings ONLY
-urlpatterns = patterns(
+urlpatterns = include(
     'cpovc_manage.views',
-    url(r'^$', 'manage_home', name='manage_home'),
-    url(r'^travel/$', 'home_travel', name='home_travel'),
-    url(r'^integration/$', 'integration_home', name='integration_home'),
-    url(r'^travel/edit/(?P<id>\d+)/$', 'edit_travel', name='edit_travel'),
-    url(r'^travel/view/(?P<id>\d+)/$', 'view_travel', name='view_travel'),
-    url(r'^travel/pdf/(?P<id>\d+)/$', 'travel_report', name='travel_report'),
+    path(r'^$', 'manage_home', name='manage_home'),
+    path(r'^travel/$', 'home_travel', name='home_travel'),
+    path(r'^integration/$', 'integration_home', name='integration_home'),
+    path(r'^travel/edit/(?P<id>\d+)/$', 'edit_travel', name='edit_travel'),
+    path(r'^travel/view/(?P<id>\d+)/$', 'view_travel', name='view_travel'),
+    path(r'^travel/pdf/(?P<id>\d+)/$', 'travel_report', name='travel_report'),
     # Integrations
-    url(r'^api/(?P<case_id>[0-9A-Za-z_\-{32}\Z]+)/$',
+    path(r'^api/(?P<case_id>[0-9A-Za-z_\-{32}\Z]+)/$',
         'process_integration', name='process_integration'),
-    url(r'^doc/(?P<doc_id>\d+)/(?P<case_id>[0-9A-Za-z_\-{32}\Z]+)/$',
+    path(r'^doc/(?P<doc_id>\d+)/(?P<case_id>[0-9A-Za-z_\-{32}\Z]+)/$',
         'get_document', name='get_document'),
-    url(r'^dq/$', 'dq_home', name='dq_home'),
-    url(r'^se/$', 'se_home', name='se_home'),
-    url(r'^sedata/$', 'se_data', name='se_data'),
+    path(r'^dq/$', 'dq_home', name='dq_home'),
+    path(r'^se/$', 'se_home', name='se_home'),
+    path(r'^sedata/$', 'se_data', name='se_data'),
     # DQA
-    url(r'^dq/$', 'dq_home', name='dq_home'),
-    url(r'^dqdata/$', 'dq_data', name='dq_data'),
+    path(r'^dq/$', 'dq_home', name='dq_home'),
+    path(r'^dqdata/$', 'dq_data', name='dq_data'),
 )
