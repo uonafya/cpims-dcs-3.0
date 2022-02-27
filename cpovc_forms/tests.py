8453bf871829046463180017d74e5cee8a917039
WalterBanda <walterkaimurima@gmail.com>
2022-02-27 03:20:03 +0300

    cpovc_forms view tests

--- cpovc_forms/tests.py
+++ cpovc_forms/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+# Form home test
+class ViewTests(TestCase):
+    def test_home_form(self):
+        response = self.client.get("forms/forms_home",{'person_type': 'TBVC'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_form_registry(self):
+        response = self.client.get("forms/forms_registry", {'form': {'placeholder': ('CCO/XX/XXX/5/29/XX/YYYY'),
+               'class': 'form-control',
+               'id': 'case_serial',
+               'data-parsley-required': 'false'}})
+        self.assertEqual(response.status_code, 200)
