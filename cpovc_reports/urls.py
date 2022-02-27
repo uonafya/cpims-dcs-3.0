1ef27c92bf5a83bf55a09fddab232de583ade5ba
Amos-Ditto <amoskip@kabarak.ac.ke>
2022-02-27 10:31:25 +0300

    cpovc_reports urls.py update

--- cpovc_reports/urls.py
+++ cpovc_reports/urls.py
@@ -1,46 +1,54 @@
-"""Urls for reports."""
-from django.conf.urls import patterns, url
+from django.urls import path # conf.urls removed
+from cpovc_reports.views import (
+    reports_home, reports_home, cpims_document, reports_cpims, reports_caseload, manage_reports,
+    manage_dashboard, reports_download, print_pdf, reports_generate, reports_pivot, reports_rawdata,
+    reports_ovc_pivot, reports_ovc_pepfar, reports_ovc_kpi, reports_ovc_rawdata, reports_ovc_download,
+    reports_ovc, dashboard_details, cluster, get_docs
+)
 
-# This should contain urls related to reports ONLY
-urlpatterns = patterns(
-    'cpovc_reports.views',
-    url(r'^$', 'reports_home', name='reports'),
-    url(r'^documents/$', 'reports_home',
+# changed to latest 4.2.0 paths
+
+urlpatterns = [
+    path(r'^$', reports_home, name='reports'),
+    path(r'^documents/$', reports_home,
         name='document_reports'),
-    url(r'^documents/(?P<doc_id>[A-Z{3}\Z]+)/(?P<case_id>[0-9A-Za-z_\-{32}\Z]+)/$', 'cpims_document',
+    path(r'^documents/(?P<doc_id>[A-Z{3}\Z]+)/(?P<case_id>[0-9A-Za-z_\-{32}\Z]+)/$', cpims_document,
         name='cpims_document'),
-    url(r'^(?P<id>\d+)/$', 'reports_cpims',
+    path(r'^(?P<id>\d+)/$', reports_cpims,
         name='cpims_reports'),
-    url(r'^caseload/$', 'reports_caseload',
+    path(r'^caseload/$', reports_caseload,
         name='caseload_reports'),
-    url(r'^manage/$', 'manage_reports',
+    path(r'^manage/$', manage_reports,
         name='manage_reports'),
-    url(r'^dashboard/$', 'manage_dashboard',
+    path(r'^dashboard/$', manage_dashboard,
         name='manage_dashboard'),
-    url(r'^download/(?P<file_name>[0-9A-Za-z_\.=\-\(\)\' ]+)$',
-        'reports_download', name='download_reports'),
-    url(r'^pdf/(?P<file_name>[0-9A-Za-z_\.=\-\(\)\' ]+)$', 'print_pdf', name='print_pdf'),
-    url(r'^generate/$', 'reports_generate',
+    path(r'^download/(?P<file_name>[0-9A-Za-z_\.=\-\(\)\' ]+)$',
+        reports_download, name='download_reports'),
+    path(r'^pdf/(?P<file_name>[0-9A-Za-z_\.=\-\(\)\' ]+)$', print_pdf, name='print_pdf'),
+    path(r'^generate/$', reports_generate,
         name='generate_reports'),
-    url(r'^pivot/$', 'reports_pivot',
+    path(r'^pivot/$', reports_pivot,
         name='pivot_reports'),
-    url(r'^data/$', 'reports_rawdata',
+    path(r'^data/$', reports_rawdata,
         name='pivot_rawdata'),
-    url(r'^datim/$', 'reports_ovc_pivot',
+    path(r'^datim/$', reports_ovc_pivot,
         name='pivot_ovc_reports'),
-    url(r'^pepfar/$', 'reports_ovc_pepfar',
+    path(r'^pepfar/$', reports_ovc_pepfar,
         name='pivot_ovc_pepfar'),
-    url(r'^kpi/$', 'reports_ovc_kpi',
+    path(r'^kpi/$', reports_ovc_kpi,
         name='pivot_ovc_kpi'),
-    url(r'^ovcdata/$', 'reports_ovc_rawdata',
+    path(r'^ovcdata/$', reports_ovc_rawdata,
         name='pivot_ovc_rawdata'),
-    url(r'^download/$', 'reports_ovc_download',
+    path(r'^download/$', reports_ovc_download,
         name='ovc_download'),
-    url(r'^ovc/(?P<id>\d+)/$', 'reports_ovc',
+    path(r'^ovc/(?P<id>\d+)/$', reports_ovc,
         name='reports_ovc'),
-    url(r'^dashboard/data/$', 'dashboard_details',
+    path(r'^dashboard/data/$', dashboard_details,
         name='dashboard_details'),
-    url(r'^cluster/$', 'cluster',
+    path(r'^cluster/$', cluster,
         name='cluster'),
-    url(r'^docs/(?P<id>\d+)/$', 'get_docs',
-        name='get_docs'),)
+    path(r'^docs/(?P<id>\d+)/$', get_docs,
+        name='get_docs')
+]
+
+# ----------------------------------------------------------#
