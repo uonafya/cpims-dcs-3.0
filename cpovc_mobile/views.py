c6af0c24975c9d8cbc714f9162101c057606a1f9
bmugwe <bonfacemugweh@gmail.com>
2024-06-22 18:24:05 +0300

    Preview csr from mobile complete

--- cpovc_mobile/views.py
+++ cpovc_mobile/views.py
@@ -313,216 +313,97 @@ def ovc_mobile_forms(request):
 @login_required
 def mobile_app_home(request):
     print(f"user id id {request.user.id}")
-    context = {
-        "cases": [
-            {
-                "id": "749fc18f2cbf11efb24922a3499bfe31",
-                "care_id": "749fc18f2cbf11efb24922a3499bfe33",
-                "case": "CCO/47/287/5/29/4423/2024",
-                "first_name": "Boniface",
-                "case_date": "2024-01-01",
-                "case_level": 2,
-            },
-            {
-                "id": "749fc18f2cbf11efb24922a3499bfe32",
-                "care_id": "2323",
-                "case": "CCO/47/287/5/29/4425/2024",
-                "first_name": "John",
-                "case_date": "2024-01-01",
-                "case_level": 2,
-            },
-            {
-                "id": "749fc18f2cbf11efb24922a3499bfe33",
-                "care_id": "2323",
-                "case": "CCO/47/287/5/29/4426/2024",
-                "first_name": "Jane",
-                "case_date": "2024-01-01",
-                "case_level": 2,
-            },
-            {
-                "id": "749fc18f2cbf11efb24922a3499bfe33",
-                "care_id": "2323",
-                "case": "CCO/47/287/5/29/4427/2024",
-                "first_name": "Jude",
-                "case_date": "2024-01-01",
-                "case_level": 2,
-            },
-        ]
-    }
+    context = { }
     return render(request, "mobile/home.html", context=context)
 
 
 def view_mobile_case_record_sheet(request, id):
-    csr_id = id
-    print(f"CSR-ID: {csr_id}")
-    context = {
+    if request.method == 'GET':
+            account_id = request.user.id
+            queryset = OVCBasicCRSMobile.objects.filter(account=account_id,is_accepted=1).values()
+            case_id = id
+            
+            check_fields = ['sex_id',
+                        'family_status_id',
+                        'household_economics',
+                        'mental_condition_id',
+                        'mental_subcondition_id',
+                        'physical_condition_id',
+                        'physical_subcondition_id',
+                        'other_condition_id',
+                        'other_subcondition_id',
+                        'case_reporter_id',
+                        'perpetrator_status_id',
+                        'relationship_type_id',
+                        'perpetrator_id', 'risk_level_id',
+                        'referral_destination_id',
+                        'intervention_id',
+                        'case_category_id', 'core_item_id',
+                        'yesno_id', 'relationship_type_id',
+                        'case_reporter_relationship_to_child',
+                        'long_term_support_id',
+                        'immediate_need_id']
+
+            vals = get_dict(field_name=check_fields)
+        
+        
+            if case_id:
+                queryset = queryset.filter(case_id=case_id,account=account_id,is_accepted=1)
+            response_list = []
+            if queryset:
+                for  query in queryset:
+                    cases = query
+                    
+               
+                    qs = OVCBasicCategoryMobile.objects.filter(case_id=case_id)
+                    ps = OVCBasicPersonMobile.objects.filter(case_id=case_id)
+                    categories = list(qs.values())
+                    persons = list(ps.values())
+                    cases['categories'] = []
+                    cases['caregivers'] = []
+                    cases['perpetrators'] = []
+                    cases['children'] = []
+                    cases['reporters'] = []
+                    cases['county_name'] = SetupGeography.objects.filter(area_id=cases['county']).values_list('area_name', flat=True).first()
+                    cases['constituency_name'] = SetupGeography.objects.filter(area_id=cases['constituency']).values_list('area_name', flat=True).first()
+                    
+                    if cases['app_form_metadata']:
+                        cases['app_form_metadata'] = json.loads(cases['app_form_metadata'].replace("'", "\""))
+                    if cases['case_params']:
+                        case_params = cases['case_params'].replace("None", "null")
+                        case_params= case_params.replace("'", "\"")
+                        cases['case_params'] = json.loads(case_params)
+                        cases['case_params']['immediate_needs'] = cases['case_params']['immediate_needs'].split(',')
+                        cases['case_params']['long_term_needs'] = cases['case_params']['long_term_needs'].split(',')
+                    del cases['is_void']
+                    for category in categories:
+                        del category['category_id']
+                        del category['is_void']
+                        cases['categories'].append(category)
+                    for person in persons:
+                        del person['case_id']
+                        del person['person_id']
+                        del person['is_void']
+                        ptype = person['person_type']
+                        if ptype == 'PTPD':
+                            cases['perpetrators'].append(person)
+                        if ptype == 'PTCG':
+                            cases['caregivers'].append(person)
+                        if ptype == 'PTCH':
+                            cases['children'].append(person)
+                        if ptype == 'PTRD':
+                            cases['reporters'].append(person)
+                    response_list.append(cases)
        
+    context = {
+        'vals': vals,
+        "ovc": response_list[0],
+        
     }
+    # breakpoint()
+    print(f"sent context: {context} ----")
     return render(request, "mobile/preview_csr.html", context=context)
-    # try:
-    #     """Get Initial Data"""
-    #     # f = FormsLog.objects.get(form_id=id, is_void=False)
-    #     ovccr = OVCCaseRecord.objects.get(case_id=id, is_void=False)
-    #     person_id = int(ovccr.person_id)
-    #     # init_data = RegPerson.objects.filter(pk=person_id)
-    #     appuser = AppUser.objects.filter(pk=ovccr.created_by).first()
-    #     f = {'form_id': id}
-
-    #     # Get Siblings
-    #     init_data = RegPerson.objects.filter(pk=person_id)
-    #     reg_personsiblings = []
-
-    #     regpersonsiblings = RegPersonsSiblings.objects.filter(
-    #         child_person_id=person_id)
-    #     for regpersonsibling in regpersonsiblings:
-    #         reg_personsiblings.append(regpersonsibling.sibling_person)
-    #     init_data.siblingpersons = reg_personsiblings
-
-    #     reg_caregivers = []
-    #     caregivers = RegPersonsGuardians.objects.filter(
-    #         child_person_id=person_id)
-    #     for caregiver in caregivers:
-    #         caregiver_details = caregiver.guardian_person
-    #         setattr(caregiver_details, 'relation', caregiver.relationship)
-    #         reg_caregivers.append(caregiver_details)
-    #     init_data.caregivers = reg_caregivers
-
-    #     check_fields = ['sex_id',
-    #                     'family_status_id',
-    #                     'household_economics',
-    #                     'mental_condition_id',
-    #                     'mental_subcondition_id',
-    #                     'physical_condition_id',
-    #                     'physical_subcondition_id',
-    #                     'other_condition_id',
-    #                     'other_subcondition_id',
-    #                     'case_reporter_id',
-    #                     'perpetrator_status_id',
-    #                     'relationship_type_id',
-    #                     'perpetrator_id', 'risk_level_id',
-    #                     'referral_destination_id',
-    #                     'intervention_id',
-    #                     'case_category_id', 'core_item_id',
-    #                     'yesno_id', 'relationship_type_id',
-    #                     'case_reporter_relationship_to_child',
-    #                     'long_term_support_id',
-    #                     'immediate_need_id']
-
-    #     vals = get_dict(field_name=check_fields)
-
-    #     ovcd = OVCEconomicStatus.objects.get(case_id=id, is_void=False)
-    #     ovcfam = OVCFamilyStatus.objects.filter(case_id=id, is_void=False)
-    #     ovccr = OVCCaseRecord.objects.get(case_id=id, is_void=False)
-    #     ovcgeo = OVCCaseGeo.objects.get(case_id=id, is_void=False)
-    #     ovcfrnds = OVCFriends.objects.filter(case_id=id, is_void=False)
-    #     ovchobs = OVCHobbies.objects.filter(case_id=id, is_void=False)
-    #     ovcmed = OVCMedical.objects.get(case_id=id, is_void=False)
-    #     ovcccats = OVCCaseCategory.objects.filter(case_id=id, is_void=False)
-    #     ovcneeds = OVCNeeds.objects.filter(case_id=id, is_void=False)
-    #     ovcrefa = OVCReferral.objects.filter(case_id=id, is_void=False)
-    #     perpetrators = OvcCasePersons.objects.filter(
-    #         case_id=id, person_type='PERP')
-    #     ovclocs = OVCCaseLocation.objects.filter(case_id=id, is_void=False)
-
-    #     # Retrieve Medical Subconditions
-    #     medical_id = ovcmed.medical_id
-    #     ovcphymeds = OVCMedicalSubconditions.objects.filter(
-    #         medical_id=medical_id, medical_condition='Physical', is_void=False)
-    #     ovcmentmeds = OVCMedicalSubconditions.objects.filter(
-    #         medical_id=medical_id, medical_condition='Mental', is_void=False)
-    #     ovcothermeds = OVCMedicalSubconditions.objects.filter(
-    #         medical_id=medical_id, medical_condition='Other', is_void=False)
-
-    #     # Get OVCCaseCategory
-    #     case_grouping_ids = []
-    #     jsonCategorysData = []
-    #     jsonSubCategorysData = []
-    #     str_jsonsubcategorydata = ''
-    #     resultsets = []
-    #     ovcccats = OVCCaseCategory.objects.filter(
-    #         case_id=id, is_void=False)
-    #     """ Get case_grouping_ids[] """
-    #     for ovcccat in ovcccats:
-    #         case_grouping_id = str(ovcccat.case_grouping_id)
-    #         if not case_grouping_id in case_grouping_ids:
-    #             case_grouping_ids.append(str(case_grouping_id))
-
-    #     """ Get Case Categories """
-    #     ovcccats2 = None
-    #     for case_grouping_id in case_grouping_ids:
-    #         ovcccats2 = OVCCaseCategory.objects.filter(
-    #             case_grouping_id=case_grouping_id)
-
-    #         for ovcccat in ovcccats2:
-    #             # OVCCaseSubCategory
-    #             ovccasesubcategorys = OVCCaseSubCategory.objects.filter(
-    #                 case_grouping_id=case_grouping_id)
-    #             for ovccasesubcategory in ovccasesubcategorys:
-    #                 jsonSubCategorysData.append(
-    #                     translate(str(ovccasesubcategory.sub_category_id)))
-    #             str_jsonsubcategorydata = ','.join(jsonSubCategorysData)
-
-    #             jsonCategorysData.append({'case_category': ovcccat.case_category,
-    #                                       'case_subcategorys': str_jsonsubcategorydata,
-    #                                       'date_of_event': (ovcccat.date_of_event).strftime('%d-%b-%Y'),
-    #                                       'place_of_event': ovcccat.place_of_event,
-    #                                       'case_nature': ovcccat.case_nature,
-    #                                       'case_grouping_id': str(ovcccat.case_grouping_id)
-    #                                       })
-    #             jsonSubCategorysData = []
-
-    #     """ Create resultsets """
-    #     resultsets.append(jsonCategorysData)
-
-    #     # Get OVCReferral
-    #     jsonData2 = []
-    #     resultsets2 = []
-    #     referral_grouping_ids = []
-    #     """ Get referral_grouping_ids[] """
-    #     for reffs in ovcrefa:
-    #         referral_grouping_id = str(reffs.referral_grouping_id)
-    #         if not referral_grouping_id in referral_grouping_ids:
-    #             referral_grouping_ids.append(str(referral_grouping_id))
-    #     """ Get Referral Actors """
-    #     ovcrefa2 = None
-    #     for referral_grouping_id in referral_grouping_ids:
-    #         ovcrefa2 = OVCReferral.objects.filter(
-    #             referral_grouping_id=referral_grouping_id)
-    #         for ra in ovcrefa2:
-    #             jsonData2.append({'refferal_actor_type': translate(ra.refferal_actor_type),
-    #                               'refferal_actor_specify': ra.refferal_actor_specify,
-    #                               'refferal_to': translate(ra.refferal_to),
-    #                               'referral_grouping_id': str(ra.referral_grouping_id)
-    #                               })
-    #     resultsets2.append(jsonData2)
-
-    #     return render(request,
-    #                   'mobile/preview_csr.html',
-    #                   {'init_data': init_data,
-    #                    'vals': vals, 'result': f,
-    #                    'ovcd': ovcd,
-    #                    'ovccr': ovccr,
-    #                    'ovcgeo': ovcgeo,
-    #                    'ovcfrnds': ovcfrnds,
-    #                    'ovchobs': ovchobs,
-    #                    'ovcmed': ovcmed,
-    #                    'ovcphymeds': ovcphymeds,
-    #                    'ovcmentmeds': ovcmentmeds,
-    #                    'ovcothermeds': ovcothermeds,
-    #                    'ovcneeds': ovcneeds,
-    #                    'perpetrators': perpetrators,
-    #                    'ovcfam': ovcfam,
-    #                    'resultsets': resultsets,
-    #                    'resultsets2': resultsets2,
-    #                    'ovclocs': ovclocs.first(),
-    #                    'app_user': appuser
-    #                    })
-    # except Exception as e:
-    #     msg = 'An error occured trying to view OVCCaseRecord - %s' % (str(e))
-    #     messages.add_message(request, messages.ERROR, msg)
-    # redirect_url = reverse(mobile_app_home)
-    # return HttpResponseRedirect(redirect_url)
-    # return render(request, "mobile/home.html", {})
+    
 
 
 
